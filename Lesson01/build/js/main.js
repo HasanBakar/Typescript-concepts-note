f69aa16818e7605437272a4511144c929754073e
HasanBakar <abu.bakar.cse.uctc@gmail.com>
2023-08-29 18:29:12 +0600

    typescript introduce part

---
+++ Lesson01/build/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+const username = "Abu Bakar";
+console.log(username);
+let a = 12;
+let c = 5;
+let b = 34;
+console.log(a + b);
+console.log(b / c);
+console.log(b * c);
---
+++ Lesson01/src/main.ts
@@ -0,0 +1,9 @@
+const username = "Abu Bakar";
+
+console.log(username);
+let a:number = 12;
+let c:number = 5
+let b:number = 34;
+console.log(a + b);
+console.log(b/c);
+console.log(b*c);
